275f66716135cfcd8c02bbcb6aff104eebae8f2f
Saran <saran@saran-desktop.(none)>
2010-10-24 15:53:22 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QutinoPrototypeDevise::Application.config.secret_token = '9d1ea504da0fdc28cf9663c81ecef0962a08a7df93cf374acf486288eedee99849c35a7c82cb26be05031995dedf2060d7b4c318347b47ed41ec1cf185a9fd87'
